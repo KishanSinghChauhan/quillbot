86c6b7d64cd23b9ebd805a57da5615490d42b617
Kishan Singh <kishan.singh@squareboat.com>
2023-01-08 06:32:11 +0530

    feat: movie card ui

--- src/components/common/MovieCard/index.tsx
+++ src/components/common/MovieCard/index.tsx
@@ -1,3 +1,30 @@
-const MovieCard = () => <div>MovieCard</div>;
+import { ReactComponent as AddIcon } from 'assets/add.svg';
+import { ReactComponent as PlayIcon } from 'assets/play.svg';
+import clsx from 'clsx';
+import { FC } from 'react';
+
+import styles from './styles.module.scss';
+
+type MovieCardProps = {
+	imgUrl?: string;
+	title?: string;
+};
+
+const MovieCard: FC<MovieCardProps> = ({ imgUrl, title }) => (
+	<div className={clsx(styles.card)}>
+		<img src={imgUrl} alt={title} />
+		<p>{title}</p>
+		<div className={styles.icons}>
+			<PlayIcon />
+			<AddIcon />
+		</div>
+	</div>
+);
 
 export default MovieCard;
+
+MovieCard.defaultProps = {
+	title: 'Free guy',
+	imgUrl:
+		'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+};
--- src/views/Common/Discover/index.tsx
+++ src/views/Common/Discover/index.tsx
@@ -1,3 +1,15 @@
-const Discover = () => <div>Discover</div>;
+import MovieCard from 'components/common/MovieCard';
+
+import styles from './styles.module.scss';
+
+const Discover = () => (
+	<div className={styles.movieList}>
+		{[1, 23, 4, 5, 6, 556, 7098, 990].map(d => (
+			<div className={styles.movieCard} key={d}>
+				<MovieCard />
+			</div>
+		))}
+	</div>
+);
 
 export default Discover;
